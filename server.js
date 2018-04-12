f7c2e29f8fbc88658c59045a011d5485a7d94f96
Nick McCurdy <nick@nickmccurdy.com>
2018-04-12 10:40:00 +0000

    🍃🍈 Checkpoint ./server.js:212971/2231

--- server.js
+++ server.js
@@ -9,9 +9,33 @@ const reverse = util.promisify(dns.reverse)
 http
   .createServer(async (req, res) => {
     try {
+        if (req.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+
+    return;
+  }
+
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  )
+    } else {
+      
       const ips = _.castArray(url.parse(req.url, true).query.ip || [])
       const hostnames = _.uniq(_.flatten(await Promise.all(ips.map(ip => reverse(ip)))))
-      res.end(JSON.stringify(hostnames))
+      res.end(JSON.stringify(hostnames))}
     } catch (err) {
       res.statusCode = 500
       res.end(JSON.stringify(err))
