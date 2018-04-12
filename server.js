8de92ead2c82759e2ae6436428ba65e66673e67b
Nick McCurdy <nick@nickmccurdy.com>
2018-04-12 09:04:16 +0000

    🍓🐟 Checkpoint

--- server.js
+++ server.js
@@ -1,39 +1,20 @@
-// server.js
-// where your node app starts
+const _ = require('lodash')
+const dns = require('dns')
+const http = require('http')
+const url = require('url')
+const util = require('util')
 
-// init project
-const express = require('express')
-const app = express()
+const reverse = util.promisify(dns.reverse)
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
-
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
-})
+http
+  .createServer(async (req, res) => {
+    try {
+      const ips = _.castArray(url.parse(req.url, true).query.ip || [])
+      const hostnames = _.uniq(_.flatten(await Promise.all(ips.map(ip => reverse(ip)))))
+      res.end(JSON.stringify(hostnames))
+    } catch (err) {
+      res.statusCode = 500
+      res.end(JSON.stringify(err))
+    }
+  })
+  .listen(process.env.PORT)
