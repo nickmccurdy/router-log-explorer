00dd7b93e6e5dd70346ee1c56f76d72c288253a2
Nick McCurdy <nick@nickmccurdy.com>
2018-04-12 11:43:16 +0000

    🐯👽 Checkpoint ./server.js:212971/552 ./index.html:212971/63

--- server.js
+++ server.js
@@ -14,23 +14,24 @@ http
 
     try {
       if (req.method.toLowerCase() == 'post') {
-        new formidable.IncomingForm().parse(req, (err, fields, files) => {
-          res.writeHead(200, { 'content-type': 'text/plain' })
-          res.write('received upload:\n\n')
-          res.end(util.inspect({ fields: fields, files: files }))
+        new formidable.IncomingForm().parse(req, (err, _, files) => {
+          res.setHeader('content-type', 'text/plain')
+          res.end(util.inspect(files.upload))
         })
       } else if (query.ip) {
-        const ips = _.castArray(query.ip || [])
+        const ips = _.castArray(query.ip)
         const hostnames = _.uniq(
           _.flatten(await Promise.all(ips.map(ip => reverse(ip))))
         )
         res.end(JSON.stringify(hostnames))
       } else {
-        fs.createReadSteam('./index.html').pipe(res)
+        res.setHeader('content-type', 'text/html')
+        fs.createReadStream('./index.html').pipe(res)
       }
     } catch (err) {
-      res.statusCode = 500
-      res.end(JSON.stringify(err))
+      throw err
+      // res.statusCode = 500
+      // res.end(JSON.stringify(err))
     }
   })
   .listen(process.env.PORT)
