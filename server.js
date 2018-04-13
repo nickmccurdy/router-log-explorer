b10a09ab2d060c566b8ae95dca381a58b92b8504
Glitch (literate-carriage) <none>
2018-04-13 10:29:16 -0400

    Parse HTML with JSDOM

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const _ = require('lodash')
+const { JSDOM } = require('jsdom')
 const dns = require('dns')
 const formidable = require('formidable')
 const fs = require('fs')
 const http = require('http')
-const pdfjs = require('pdfjs-dist')
 const url = require('url')
 const util = require('util')
 
@@ -16,17 +16,22 @@ http
         new formidable.IncomingForm().parse(req, async (err, fields, files) => {
           if (err) throw err
 
-          const pdfDocument = await pdfjs.getDocument(files.upload.path)
-          const contents = await Promise.all(
-            _.range(1, pdfDocument.numPages + 1).map(async n => {
-              const page = await pdfDocument.getPage(n)
-              return page.getTextContent()
-            })
-          )
+          const dom = await JSDOM.fromFile(files.upload.path)
           res.setHeader('content-type', 'application/json')
           res.end(
             JSON.stringify(
-              _.flatMap(contents, content => _.map(content.items, 'str'))
+              _.chunk(
+                _.compact(
+                  _.map(
+                    _.map(
+                      dom.window.document.querySelectorAll('td'),
+                      'textContent'
+                    ),
+                    _.trim
+                  )
+                ),
+                3
+              )
             )
           )
         })
