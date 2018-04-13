3e8360e03926affdcb009c1f8a6d5d5fcf9331b8
Nick McCurdy <nick@nickmccurdy.com>
2018-04-12 21:51:32 -0400

    Parse all pages into a JSON Array

--- server.js
+++ server.js
@@ -15,15 +15,22 @@ http
       const query = url.parse(req.url, true).query
 
       if (req.method.toLowerCase() == 'post') {
-        new formidable.IncomingForm().parse(req, async (err, _, files) => {
+        new formidable.IncomingForm().parse(req, async (err, fields, files) => {
           if (err) throw err
-          // res.setHeader('content-type', 'application/pdf')
-          // fs.createReadStream(files.upload.path).pipe(res)
 
           const pdfDocument = await pdfjs.getDocument(files.upload.path)
-          const page = await pdfDocument.getPage(1)
-          const textContent = await page.getTextContent()
-          res.end(textContent.items.map(item => item.str).join(' '))
+          const contents = await Promise.all(
+            _.range(1, pdfDocument.numPages + 1).map(async n => {
+              const page = await pdfDocument.getPage(n)
+              return page.getTextContent()
+            })
+          )
+          res.setHeader('content-type', 'application/json')
+          res.end(
+            JSON.stringify(
+              _.flatMap(contents, content => _.map(content.items, 'str'))
+            )
+          )
         })
       } else if (query.ip) {
         const ips = _.castArray(query.ip)
